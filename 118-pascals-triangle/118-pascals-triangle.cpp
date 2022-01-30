edd759645dc4afd10e805ba68f617a53218193a7
kanishka-p1 <92727668+kanishka-p1@users.noreply.github.com>
2022-01-30 10:28:45 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (91.69%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> pascal(numRows);
+        
+        for(int i = 0; i < numRows; i++){
+            pascal[i].resize(i+1); //every ith row will have i+1 column
+            pascal[i][0] = pascal[i][i] = 1; // first and last column will be 1
+            for(int j = 1; j < i; j++){
+                pascal[i][j] = pascal[i-1][j-1] + pascal[i-1][j];
+            }
+        }
+        return pascal;
+    }
+};
