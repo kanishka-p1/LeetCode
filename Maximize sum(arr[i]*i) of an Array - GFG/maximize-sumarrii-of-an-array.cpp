12654e45315ef7b079ce65aa4f8e198faef3cc8e
kaystion <92727668+kanishka-p1@users.noreply.github.com>
2022-02-26 15:46:09 +0530

    Added solution - LeetHub

---
+++ Maximize sum(arr[i]*i) of an Array - GFG/maximize-sumarrii-of-an-array.cpp
@@ -0,0 +1,35 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    int Maximize(int a[],int n)
+    {
+        // Complete the function
+        sort(a, a + n);
+        long sum = 0;
+        int mod = pow(10, 9) + 7;
+        for(int i = 0; i < n; i++){
+            sum += (long)a[i] * i;
+        }
+        return sum % mod;
+    }
+};
+
+// { Driver Code Starts.
+int main(){
+
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        Solution ob;
+        cout<<ob.Maximize(a,n)<<endl;
+    }
+}  // } Driver Code Ends
