8416d109288fe8ae4a6df39954f07ea1c036ad78
kaystion <92727668+kanishka-p1@users.noreply.github.com>
2022-03-01 00:43:53 +0530

    Time: 6 ms (77.84%), Space: 7.4 MB (49.51%) - LeetHub

---
+++ 51-n-queens/51-n-queens.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<string>> res;
+    vector<vector<string>> solveNQueens(int n) {
+        if(n == 0 || n == 2 || n == 3){
+            return res;
+        }
+        vector<string> board(n, string(n, '.'));
+        findPlaces(board, 0);
+        return res;
+    }
+    void findPlaces(vector<string>& board, int row){
+        if(row == board.size()){
+            res.push_back(board);
+            return;
+        }
+        for(int i = 0; i < board.size(); i++){
+            if(canPlace(board, row, i)){
+                board[row][i] = 'Q';
+                findPlaces(board, row + 1);
+                board[row][i] = '.';
+            }    
+        }
+    }
+    bool canPlace(vector<string>& board, int row, int col){
+        for(int i = row; i >= 0; i--){
+            if(board[i][col] == 'Q')
+                return false;
+        }
+        for(int i = row, j = col; i >= 0 && j >= 0; i--, j--){
+            if(board[i][j] == 'Q')
+                return false;
+        }
+        for(int i = row, j = col; i >= 0 && j < board.size(); i--, j++){
+            if(board[i][j] == 'Q')
+                return false;
+        }
+        return true;
+    }
+};
