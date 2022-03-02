1d6c4b4665af03436f631be2e955c4bdec2691e8
kaystion <92727668+kanishka-p1@users.noreply.github.com>
2022-03-02 10:43:22 +0530

    Added solution - LeetHub

---
+++ Find the Safe Position - GFG/find-the-safe-position.cpp
@@ -0,0 +1,34 @@
+// { Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+class Solution {
+  public:
+    int safePos(int n, int k) {
+        // code here
+        if(n == 1){
+            return 1;
+        }
+        int x = safePos(n-1, k);
+        int y = (x + k - 1) % n + 1;
+        return y;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n,k;
+        
+        cin>>n>>k;
+
+        Solution ob;
+        cout << ob.safePos(n,k) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
