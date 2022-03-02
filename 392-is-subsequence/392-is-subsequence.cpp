a4c82653734b14474b519d82b9d1ddc07c9a5bb1
kaystion <92727668+kanishka-p1@users.noreply.github.com>
2022-03-02 10:40:22 +0530

    Time: 5 ms (28.86%), Space: 6.4 MB (72.95%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
+    // USING TWO POINTER
+    bool isSubsequence(string s, string t) {
+        int m = s.size();
+        int n = t.size();
+        int i = 0, j = 0;
+        while(i < m && j < n) {
+            if(s[i] == t[j]) {
+                i++;
+            }
+            j++;
+        }
+        return i == m ? 1 : 0;
+    }
     
+    /*   USING RECURSION
     bool subset(int m, int n, string s, string t){
         if(m == 0){
             return true;
@@ -20,4 +34,5 @@ public:
         int n = t.size();
         return subset(m, n, s, t);
     }
+    */
 };
