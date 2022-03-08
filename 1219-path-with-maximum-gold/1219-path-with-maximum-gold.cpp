dcd2cb391761f109d7196ec7589882ffab6da973
kaystion <92727668+kanishka-p1@users.noreply.github.com>
2022-03-09 00:47:27 +0530

    Time: 312 ms (29.31%), Space: 7.3 MB (86.96%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
@@ -1,59 +1,31 @@
-int dfs(vector<vector<int>>& grid, int i,int j)
-{
-    
-    int ans=0, temp=0;
-    if(i>=grid.size() || i<0 || j<0 || j>=grid[0].size() || grid[i][j]==0) return 0;
-    temp=grid[i][j];
-    grid[i][j]=0;
-    ans = max(ans,dfs(grid,i-1,j));
-    ans = max(ans,dfs(grid,i+1,j));
-    ans = max(ans,dfs(grid,i,j+1));
-    ans = max(ans,dfs(grid,i,j-1));
-    grid[i][j] = temp;
-    return ans+temp;
-}
-
 class Solution {
 public:
-    int getMaximumGold(vector<vector<int>>& grid){
-        
-        int ans=0;
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j]>0){
-                    ans = max(ans,dfs(grid,i,j));
-                }
-            }
-        }
-        return ans;
-    }
-};
-
-/*class Solution {
-public:
     
-    int maxgold(vector<vector<int>> grid, int i, int j){
-        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] <= 0){
+    int maxgold(vector<vector<int>>& grid, int i, int j){
+        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == 0){
             return 0;
         }
-        grid[i][j] = -grid[i][j];
         int ans = 0;
+        int temp = grid[i][j];
+        grid[i][j] = 0;
+        ans = max(ans, maxgold(grid, i - 1, j));
         ans = max(ans, maxgold(grid, i + 1, j));
         ans = max(ans, maxgold(grid, i, j + 1));
-        ans = max(ans, maxgold(grid, i - 1, j));
         ans = max(ans, maxgold(grid, i, j - 1));
-        grid[i][j] = -grid[i][j];
+        grid[i][j] = temp;
         
-        return grid[i][j] + ans;
+        return temp + ans;
     }
     
     int getMaximumGold(vector<vector<int>>& grid) {
         int sum = 0;
         for(int i = 0; i < grid.size(); i++){
             for(int j = 0; j < grid[0].size(); j++){
-                sum = max(sum, maxgold(grid, i, j));
+                if(grid[i][j] > 0){
+                    sum = max(sum, maxgold(grid, i, j));                    
+                }
             }
         }
         return sum;
     }
-};*/
+};
