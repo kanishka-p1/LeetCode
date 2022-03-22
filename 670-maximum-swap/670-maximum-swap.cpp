83385c3db83a2af1e5dd3cd69d1981250ea979bb
kaystion <92727668+kanishka-p1@users.noreply.github.com>
2022-03-23 01:04:05 +0530

    Time: 4 ms (21.29%), Space: 6 MB (59.88%) - LeetHub

--- 670-maximum-swap/670-maximum-swap.cpp
+++ 670-maximum-swap/670-maximum-swap.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*using map
     int maximumSwap(int num) {
         string n = to_string(num);
         unordered_map<int, int> flag;
@@ -12,6 +13,29 @@ public:
                     swap(n[flag[j]], n[i]);
                     return stoi(n);
                 }
+            }
+        }
+        return stoi(n);
+    }*/
+    
+    //using DP
+    int maximumSwap(int num){
+       string n = to_string(num);
+        int s = n.size();
+        vector<int> dp(s, -1);
+        
+        int currmax = s - 1;  
+        for(int i = s - 1; i >= 0; i--){
+            if(n[i] > n[currmax]){
+                currmax = i;
+            }
+            dp[i] = currmax;
+        }
+        
+        for(int i = 0; i < s; i++){
+            if(n[dp[i]] != n[i]){
+                swap(n[dp[i]], n[i]);
+                break;
             }
         }
         return stoi(n);
