be2666b0e5cff20b826b8b2c82a3e7a1d7baee37
kaystion <92727668+kanishka-p1@users.noreply.github.com>
2022-06-05 12:20:14 +0530

    Create return_permutation.cpp

---
+++ 46-permutations/return_permutation.cpp
@@ -0,0 +1,23 @@
+#include <string>
+using namespace std;
+
+int returnPermutations(string input, string output[]){
+   	/* Don't write main() function.
+	 * Don't read input, it is passed as function argument.
+	 * Print output as specified in the question
+	*/
+    if(input.length() == 0)
+    {
+        output[0] = ""; 
+        return 1;
+    }
+    string smallOutput[10000];
+    int smallSize = returnPermutations(input.substr(1), smallOutput); 
+    int k = 0;
+    for(int i = 0; i < smallSize; i++){ 
+        for(int j = 0; j <= smallOutput[i].length(); j++){ 
+            output[k++] = smallOutput[i].substr(0,j) + input[0] + smallOutput[i].substr(j);
+        }
+    }
+    return k;
+}
