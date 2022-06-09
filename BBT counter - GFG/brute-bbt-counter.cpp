909e7a97b95e2227b802e04c358c9483e7d2acae
kaystion <92727668+kanishka-p1@users.noreply.github.com>
2022-06-09 15:58:43 +0530

    Create brute-bbt-counter.cpp

---
+++ BBT counter - GFG/brute-bbt-counter.cpp
@@ -0,0 +1,19 @@
+#include<cmath>
+
+int balancedBTs(int n) {
+    // Write your code here
+    if(n <= 1){
+        return 1;
+    }
+    
+    int x = balancedBTs(n-1);
+    int y = balancedBTs(n-2);
+    
+    int mod = (int)(pow(10,9) + 7);
+    
+    int temp1 = (int) (((long)(x)*x)%mod);
+    int temp2 = (int) ((2*(long)(x)*y)%mod);
+    
+    int ans = (temp1+temp2)%mod;
+    return ans;
+}
